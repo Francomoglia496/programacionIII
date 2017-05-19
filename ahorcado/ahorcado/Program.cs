420f030dd5d439c210aa5b8f3136699986774642
Francomoglia496 <francomoglia496@gmail.com>
2017-05-19 11:00:46 -0300

    Add files via upload

---
+++ ahorcado/ahorcado/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AhorcadoPalabras;
+
+namespace ahorcado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string palabra = new AhorcadoPalabras.GeneradorPalabras().SiguientePalabra;
+
+            string[] palabraFinal = new string[palabra.Length];
+
+            Console.WriteLine(palabra);
+
+            for (int i = 0; i < palabra.Length; i++)
+            {
+                palabraFinal[i] = "_ "; 
+            }
+
+
+            Console.WriteLine("La palabra contiene: ");
+            foreach (string a in palabraFinal)
+            {
+                Console.Write(a);
+            }
+
+            Console.WriteLine("");
+
+            int errores = 0;
+
+            string ultimo = "";
+            Boolean iguales = false;
+
+            while(errores < 6 && iguales != true){
+
+                Console.WriteLine("Ingrese una letra: ");
+                char letra = char.Parse(Console.ReadLine());
+
+
+                int cont = 0;
+
+                for (int j = 0; j < palabra.Length; j++)
+                {
+
+                    if (palabra[j].Equals(letra))
+                    {
+                        palabraFinal[j] = letra + "";
+                    }
+                    else {
+                        cont++;
+                    }
+
+
+                }
+
+                foreach (string str in palabraFinal)
+                {
+                    Console.Write(str);
+                }
+
+                Console.WriteLine("");
+                Console.WriteLine("-----------------------");
+
+                if (cont == palabra.Length)
+                {
+                    errores++;
+                }
+
+                Console.WriteLine(errores);
+
+                foreach (string k in palabraFinal)
+                {
+                    ultimo = ultimo + k;
+                }
+
+                int cont3 = 0;
+                for (int i = 0; i < palabraFinal.Length; i++)
+                {
+                    if (palabraFinal[i].Equals(ultimo[i]))
+                    {
+                        cont3++;
+                    }
+                }
+
+                if (cont3 == palabraFinal.Length)
+                {
+                    iguales = true;
+                }
+
+            }
+
+            int cont2 = 0;
+            foreach (string x in palabraFinal)
+            {
+                if (!x.Equals("_ "))
+                {
+                    cont2++;
+                }
+
+            }
+
+            Console.WriteLine("cont2: " + cont2);
+
+            if (cont2 == palabraFinal.Length)
+            {
+                Console.WriteLine("ganaste");
+            }
+            else if (errores == 6) {
+                Console.WriteLine("Perdiste");
+            }
+
+
+
+            Console.WriteLine(palabra);
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ encriptar/prog3_12_5_2017/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prog3_12_5_2017
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //string a = "Mb fcyb dhrqb zbFgéñégr yñ dhRégñ, gi rérf ehüra yñ gÜrar ehr ñgéñjrFñé. Üvxaéalaosevélv vjvk vd vdvyaüg.";
+            string texto = "Mb fcyb dhrqb zbFgéñégr yñ dhRégñ, gi rérf ehüra yñ gÜrar ehr ñgéñjrFñé.";
+            string texto1 = "Üvxaéalaosevélv vjvk vd vdvyaüg.";
+            int clave = 17;
+            int clave2 = 10;
+
+            Desencriptar(texto, clave);
+            Desencriptar(texto1, clave2);
+
+            Console.ReadKey();
+
+        }
+
+        static void Desencriptar(string texto,int clave) {
+
+            string texto1 = texto.ToLower();
+
+            for (int i = 0; i < texto1.Length; i++)
+            {
+                //string[] aux = { "a", "á", "b", "c", "d", "e", "é", "f", "g", "h", "i", "í", "j",
+                //                   "k", "l", "m", "n", "o","ó", "p", "q", "r", "s", "t", "u","ú", "v", "w", "x", "y", "z" }; äëïöü
+                string aux1 = "aábcdeéfghiíjklmnñoópqrstuúüvwxyzAÁBCDEÉFGHIÍJKLMNÑOÓPQRSTUÚÜVWXYZ";
+                if (aux1.Contains(texto1[i]))
+                {
+
+                    int index = aux1.IndexOf(texto1[i]);
+
+                    int claveFinal = index + clave;
+
+                    while (claveFinal >= aux1.Length)
+                    {
+                        claveFinal = claveFinal - aux1.Length;
+                    }
+
+                    //Console.WriteLine(texto1[i]+" -> tiene");
+                    Console.Write("{0}",aux1[claveFinal]);
+
+                }
+                else
+                {
+                    //Console.WriteLine("no tiene");
+                    Console.Write("{0}",texto1[i]);
+                }
+
+            }
+        }
+    }
+}
