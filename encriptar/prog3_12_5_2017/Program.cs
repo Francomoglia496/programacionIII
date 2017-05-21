f2fbf499bc8158816032dc205f973f68ffb52fba
Francomoglia496 <francomoglia496@gmail.com>
2017-05-21 20:37:52 -0300

    actualizado
    actualizado

--- encriptar/prog3_12_5_2017/Program.cs
+++ encriptar/prog3_12_5_2017/Program.cs
@@ -13,12 +13,12 @@ namespace prog3_12_5_2017
 
             //string a = "Mb fcyb dhrqb zbFgéñégr yñ dhRégñ, gi rérf ehüra yñ gÜrar ehr ñgéñjrFñé. Üvxaéalaosevélv vjvk vd vdvyaüg.";
             string texto = "Mb fcyb dhrqb zbFgéñégr yñ dhRégñ, gi rérf ehüra yñ gÜrar ehr ñgéñjrFñé.";
-            string texto1 = "Üvxaéalaosevélv vjvk vd vdvyaüg.";
-            int clave = 17;
+            string texto2 = "Üvxaéalaosevélv vjvk vd vdvyaüg.";
+            int clave = 16;
             int clave2 = 10;
 
             Desencriptar(texto, clave);
-            Desencriptar(texto1, clave2);
+            Desencriptar(texto2, clave2);
 
             Console.ReadKey();
 
@@ -32,7 +32,8 @@ namespace prog3_12_5_2017
             {
                 //string[] aux = { "a", "á", "b", "c", "d", "e", "é", "f", "g", "h", "i", "í", "j",
                 //                   "k", "l", "m", "n", "o","ó", "p", "q", "r", "s", "t", "u","ú", "v", "w", "x", "y", "z" }; äëïöü
-                string aux1 = "aábcdeéfghiíjklmnñoópqrstuúüvwxyzAÁBCDEÉFGHIÍJKLMNÑOÓPQRSTUÚÜVWXYZ";
+                string aux1 = "aábcdeéfghiíjklmnñoópqrstuúüvwxyz";
+                string aux2 = "AÁBCDEÉFGHIÍJKLMNÑOÓPQRSTUÚÜVWXYZ";
                 if (aux1.Contains(texto1[i]))
                 {
 
@@ -49,10 +50,25 @@ namespace prog3_12_5_2017
                     Console.Write("{0}",aux1[claveFinal]);
 
                 }
-                else
+                else if (aux2.Contains(texto1[i]))
                 {
+
+                    int index = aux2.IndexOf(texto1[i]);
+
+                    int claveFinal = index + clave;
+
+                    while (claveFinal >= aux1.Length)
+                    {
+                        claveFinal = claveFinal - aux1.Length;
+                    }
+
+                    //Console.WriteLine(texto1[i]+" -> tiene");
+                    Console.Write("{0}", aux2[claveFinal]);
+
+                }
+                else {
                     //Console.WriteLine("no tiene");
-                    Console.Write("{0}",texto1[i]);
+                    Console.Write("{0}", texto1[i]);
                 }
 
             }
