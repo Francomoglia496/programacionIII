fd92af04a2f40b571886e14982c92ab08105f1d4
umantram <umantram@gmail.com>
2017-06-20 18:58:00 -0300

    agregado el codigo de la DLL Agente

---
+++ AgenteDLL/AgenteDLL/Agente.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgenteDLL
+{
+    public class Agente
+    {
+
+        String nombre;
+
+        public string Nombre
+        {
+            get
+            {
+                return nombre;
+            }
+
+            set
+            {
+                nombre = value;
+            }
+        }
+
+        public string Imagen
+        {
+            get
+            {
+                return imagen;
+            }
+
+            set
+            {
+                imagen = value;
+            }
+        }
+
+        String imagen;
+
+
+
+    }
+}
